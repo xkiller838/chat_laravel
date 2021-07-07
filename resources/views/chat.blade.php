46bc6232eb3c658ff8c64de0f4be828c8dbc00e7
Manuel <33810066+xkiller838@users.noreply.github.com>
2021-07-07 15:27:48 -0500

    ajustando el nombre

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 	<head>
 		<meta charset="UTF-8">
-		<title>Chat killer</title>
+		<title>Chat killerWeb</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
